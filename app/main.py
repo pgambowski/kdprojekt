302270ff943a2f6a102bc33ebf170ccc7fd8eb13
pgambowski <gambowski.piotr@gmail.com>
2020-07-08 19:18:17 +0200

    fix db model and inserting data

--- app/main.py
+++ app/main.py
@@ -1,28 +1,40 @@
-from flask import Blueprint, render_template, request
-from .models import Contract
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Contract, PersonalData
 from. import db
 
 main= Blueprint('main', __name__)
 
+@main.route('/')
+def index():
+    return redirect('/kdprojekt')
 
 @main.route('/kdprojekt')
 @main.route('/urcup')
-def index():
+def form():
     rule = request.url_rule
     if 'kdprojekt' in rule.rule:
         company = True
     elif 'urcup' in rule.rule:
         company = False
-    return render_template('index.html', company=company)
+    return render_template('form.html', company=company)
 
 @main.route('/submit', methods=['POST'])
 def submit():
-    reasons = request.form.getlist('insuranceReason')
-    data = Contract(reasons=reasons)
-    db.session.add(data)
+    data = request.form
+    print(data)
+    contract = Contract(company=data['company']) 
+    db.session.add(contract)
+    
+    personal_data = PersonalData(contract=contract, surname=data['surname'], name=data['name'],
+    second_name=data['secondName'],  father_name=data['fathersName'],
+    mother_name=data['mothersName'], birthday=data['birthday'], birth_place=data['birthPlace'],
+    family_name=data['familyName'], citizenship=data['citizenship'], pesel=data['pesel'],
+    nip=data['nip'], phone_number=data['phoneNumber'], email=data['email'], bank_name=data['bankName'],
+    account_number=data['accountNumber'], stay_number=data['stayNumber'])
+    db.session.add(personal_data)
+    
     db.session.commit()
-
-    return render_template('index.html')
+    return redirect('/kdprojekt')
 
 @main.route('/manage')
 def manage_view():
--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.dialects import postgresql
 
 class Contract(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    create_date = db.Column(db.DateTime, nullable=False,  server_default=db.func.now())
+    create_date = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
     company = db.Column(db.String(10), nullable=False)
     personal_data = db.relationship('PersonalData', backref='contract', lazy='dynamic')
     
@@ -14,13 +14,14 @@ class Contract(db.Model):
 
 class PersonalData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    contract_id = db.Column(db.Integer, db.ForeignKey('contract.id'), nullable=False)
+    contract_id = db.Column(db.Integer, db.ForeignKey('contract.id'))
     surname = db.Column(db.String(80), nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     second_name = db.Column(db.String(50))
     father_name = db.Column(db.String(50), nullable=False)
     mother_name = db.Column(db.String(50), nullable=False)
-    birth_date = db.Column(db.String(20), nullable=False)
+    birthday = db.Column(db.String(20), nullable=False)
+    birth_place = db.Column(db.String(100), nullable=False)
     family_name = db.Column(db.String(80))
     citizenship = db.Column(db.String(50))
     pesel = db.Column(db.String(11), nullable=False)
@@ -28,9 +29,9 @@ class PersonalData(db.Model):
     phone_number = db.Column(db.String(11), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     bank_name = db.Column(db.String(100))
-    bank_number = db.Column(db.String(32))
-    stay_card_number = db.Column(db.String(100))
+    account_number = db.Column(db.String(32))
+    stay_number = db.Column(db.String(100))
 
     def __repr__(self):
-        return '<PersonalData {} {}>'.format(self.first_name, self.surname) 
+        return '<PersonalData {}>'.format(self.name) 
     
